67a965944da2df332d714eb6b5873221dbd50e96
Steven Scott <stevescott2653@gmail.com>
2019-10-01 16:46:26 -0400

    Add files via upload

---
+++ Abstract_Class/Abstract_Class/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    public abstract class Shape
+    {
+        public abstract void Draw();
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("drawing rectangle...");
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("drawing circle...");
+        }
+    }
+
+    public class TestAbstract
+    {
+        public static void Main()
+        {
+            Shape s;
+            s = new Rectangle();
+            s.Draw();
+            s =new Circle();
+            s.Draw();
+        }
+    }
+}
